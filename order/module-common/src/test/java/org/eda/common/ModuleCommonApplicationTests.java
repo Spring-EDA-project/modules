f9cc6cbc20a2b450dd4e338eb3e1945451c3bbd5
MinchoGreenT <dldmstjq99@gmail.com>
2025-02-08 15:01:57 +0900

    [feat] common 모듈 생성

---
+++ order/module-common/src/test/java/org/eda/common/ModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eda.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
