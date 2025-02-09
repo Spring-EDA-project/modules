4ee6e7da8ac8bf990d61204138ddb1364ae250fd
RyuKwanKon <rhks33rhs@gmail.com>
2025-02-09 16:31:11 +0900

    [init] init kotlin project

---
+++ pay/module-external-api/src/main/kotlin/org/eda/ExternalApiApplication.kt
@@ -0,0 +1,11 @@
+package org.eda
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExternalApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExternalApiApplication>(*args)
+}
