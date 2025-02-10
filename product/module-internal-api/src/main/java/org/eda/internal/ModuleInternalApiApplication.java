467ae71f041e458816196f96926114e5a0729a94
MinchoGreenT <dldmstjq99@gmail.com>
2025-02-10 18:21:40 +0900

    [feat] 인터널 어플리케이션 모듈 추가

---
+++ product/module-internal-api/src/main/java/org/eda/internal/ModuleInternalApiApplication.java
@@ -0,0 +1,13 @@
+package org.eda.internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleInternalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleInternalApiApplication.class, args);
+    }
+
+}
