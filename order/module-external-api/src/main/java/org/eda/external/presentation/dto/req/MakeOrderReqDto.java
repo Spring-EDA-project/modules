e821032168aa69ba320fca30e1abb25b9a61b5b6
MinchoGreenT <dldmstjq99@gmail.com>
2025-02-10 16:49:39 +0900

    [feat] 주문 시스템 api 설계

---
+++ order/module-external-api/src/main/java/org/eda/external/presentation/dto/req/MakeOrderReqDto.java
@@ -0,0 +1,13 @@
+package org.eda.external.presentation.dto.req;
+
+import java.util.List;
+
+public record MakeOrderReqDto(
+        Long memberId,
+        List<ProductItemDto> items
+) {
+    public record ProductItemDto(
+            Long productId,
+            int amount
+    ) {}
+}
---
+++ order/module-external-api/src/main/java/org/eda/external/presentation/dto/res/GetAllOrdersResDto.java
@@ -0,0 +1,18 @@
+package org.eda.external.presentation.dto.res;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.eda.domain.Order;
+
+import java.util.List;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record GetAllOrdersResDto(
+        List<Order> orders
+) {
+    public static GetAllOrdersResDto of(List<Order> orders) {
+        return GetAllOrdersResDto.builder()
+                .orders(orders)
+                .build();
+    }
+}
