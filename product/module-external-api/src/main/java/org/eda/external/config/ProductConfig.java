3b343dd9d63f8d1f5da09abeb0c86bca7b305a18
MinchoGreenT <dldmstjq99@gmail.com>
2025-02-10 18:21:01 +0900

    [feat] 어플리케이션 모듈 설정 및 인터널 어플리케이션 모듈 추가

---
+++ product/module-external-api/src/main/java/org/eda/external/config/ProductConfig.java
@@ -0,0 +1,7 @@
+package org.eda.external.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductConfig {
+}
---
+++ product/module-internal-api/src/main/java/org/eda/internal/presentation/ProductApiController.java
@@ -0,0 +1,11 @@
+package org.eda.internal.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+@RestController
+public class ProductApiController {
+}
