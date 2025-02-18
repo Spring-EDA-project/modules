002dd69f72c6b69a79bde04a29aee58b2f5dbc8a
MinchoGreenT <dldmstjq99@gmail.com>
2025-02-18 16:55:44 +0900

    [feat] rdbms 모듈 추가

---
+++ product/module-core/product-rdbms-core/src/main/java/org/eda/rdbms/ProductRdbmsCoreApplication.java
@@ -0,0 +1,13 @@
+package org.eda.rdbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRdbmsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRdbmsCoreApplication.class, args);
+	}
+
+}
