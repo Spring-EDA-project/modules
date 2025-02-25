fa00a2672edf7c82e7ed089bd1527129b0d54a95
MinchoGreenT <dldmstjq99@gmail.com>
2025-02-26 00:19:43 +0900

    [feat] 상품 단일/전체 조회 서비스 구현

--- product/module-external-api/src/main/java/com/eda/external/application/GetAllProductsService.java
+++ product/module-external-api/src/main/java/com/eda/external/application/GetAllProductsService.java
@@ -1,14 +1,19 @@
 package com.eda.external.application;
 
 import com.eda.domain.Product;
+import com.eda.domain.ProductReader;
 import com.eda.external.application.port.in.GetAllProductsUseCase;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class GetAllProductsService implements GetAllProductsUseCase {
+    private final ProductReader productReader;
+
     public List<Product> getAllProducts() {
-        return null;
+        return productReader.getAllProducts();
     }
 }
--- product/module-external-api/src/main/java/com/eda/external/application/GetProductService.java
+++ product/module-external-api/src/main/java/com/eda/external/application/GetProductService.java
@@ -1,17 +1,17 @@
 package com.eda.external.application;
 
 import com.eda.domain.Product;
+import com.eda.domain.ProductReader;
 import com.eda.external.application.port.in.GetProductUseCase;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class GetProductService implements GetProductUseCase {
+    private final ProductReader productReader;
+
     public Product getProduct(Long productId) {
-        return Product.builder()
-                .id(productId)
-                .image(null)
-                .name(null)
-                .price(0)
-                .build();
+        return productReader.getProduct(productId);
     }
 }
