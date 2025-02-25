18ffc609e584225796fc075370d737cd23b85f83
MinchoGreenT <dldmstjq99@gmail.com>
2025-02-26 00:51:21 +0900

    [feat] rdbms 모듈 추가

---
+++ order/module-core/order-rdbms-core/src/main/java/org/eda/rdbms/OrderRdbmsCoreApplication.java
@@ -0,0 +1,13 @@
+package org.eda.rdbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRdbmsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderRdbmsCoreApplication.class, args);
+    }
+
+}
