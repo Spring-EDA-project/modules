59c74f11984d9092ad3dde72247eca76fcc1e748
MinchoGreenT <dldmstjq99@gmail.com>
2025-02-26 00:19:32 +0900

    [feat] 상품 데이터 생성 로직

--- product/module-core/product-domain-core/src/main/java/com/eda/domain/ProductWriter.java
+++ product/module-core/product-domain-core/src/main/java/com/eda/domain/ProductWriter.java
@@ -1,4 +1,16 @@
 package com.eda.domain;
 
+import com.eda.rdbms.entity.ProductEntity;
+import com.eda.rdbms.repository.ports.in.ProductRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class ProductWriter {
+    private final ProductRepository productRepository;
+
+    public Product write(Product product) {
+        ProductEntity productEntity = product.toEntity();
+        ProductEntity savedProductEntity = productRepository.save(productEntity);
+        return Product.fromEntity(savedProductEntity);
+    }
 }
