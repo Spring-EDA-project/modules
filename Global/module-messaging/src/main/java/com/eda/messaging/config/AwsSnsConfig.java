7fcbf76a1ee94625cc5783916f7f60416638f940
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2025-02-27 22:25:23 +0900

    [feat] value 어노테이션 삭제

--- Global/module-messaging/src/main/java/com/eda/messaging/config/AwsSnsConfig.java
+++ Global/module-messaging/src/main/java/com/eda/messaging/config/AwsSnsConfig.java
@@ -4,23 +4,15 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
-@Getter
-@Configuration
 public class AwsSnsConfig {
-    @Value("${spring.cloud.aws.credentials.access-key}")
     private String accessKey;
-
-    @Value("${spring.cloud.aws.credentials.secret-key}")
     private String secretKey;
-
-    @Value("${spring.cloud.aws.region.static}")
     private String region;
-
-    @Value("${spring.cloud.aws.sns.topic.arn}")
     private String topicArn;
 
     @Bean
