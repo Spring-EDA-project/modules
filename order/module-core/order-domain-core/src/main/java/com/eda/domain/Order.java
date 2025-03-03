51fafe5e7bb0ec6fa4fbc1e56a660dc21e53039c
MinchoGreenT <dldmstjq99@gmail.com>
2025-03-03 15:01:02 +0900

    [feat] 도메인 매핑 메서드 생성

--- order/module-core/order-domain-core/src/main/java/com/eda/domain/Order.java
+++ order/module-core/order-domain-core/src/main/java/com/eda/domain/Order.java
@@ -1,5 +1,6 @@
 package com.eda.domain;
 
+import com.eda.rdbms.entity.OrderEntity;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,4 +13,22 @@ public class Order {
     private String uuid;
     private Long memberId;
     private List<OrderProduct> orderProducts;
+
+    public OrderEntity toOrderEntity() {
+        return OrderEntity.builder()
+                .id(id)
+                .uuid(uuid)
+                .memberId(memberId)
+                .orderProducts(this.orderProducts.stream().map(OrderProduct::toEntity).toList())
+                .build();
+    }
+
+    public static Order fromOrderEntity(OrderEntity orderEntity) {
+        return Order.builder()
+                .id(orderEntity.getId())
+                .uuid(orderEntity.getUuid())
+                .memberId(orderEntity.getMemberId())
+                .orderProducts(orderEntity.getOrderProducts().stream().map(OrderProduct::fromEntity).toList())
+                .build();
+    }
 }
--- order/module-core/order-domain-core/src/main/java/com/eda/domain/OrderProduct.java
+++ order/module-core/order-domain-core/src/main/java/com/eda/domain/OrderProduct.java
@@ -1,5 +1,6 @@
 package com.eda.domain;
 
+import com.eda.rdbms.entity.OrderProductEntity;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,4 +11,21 @@ public class OrderProduct {
     private Long orderId;
     private Long productId;
     private int amount;
+
+    public OrderProductEntity toEntity() {
+        return OrderProductEntity.builder()
+                .orderId(orderId)
+                .productId(productId)
+                .amount(amount)
+                .build();
+    }
+
+    public static OrderProduct fromEntity(OrderProductEntity entity) {
+        return OrderProduct.builder()
+                .id(entity.getId())
+                .orderId(entity.getOrderId())
+                .productId(entity.getProductId())
+                .amount(entity.getAmount())
+                .build();
+    }
 }
