aab9331753d7e4760d13d36ae5ab3f7ed745e914
wooing1084 <kmjung1515@naver.com>
2025-03-04 19:25:09 +0900

    feat: global 의존성 추가

--- Member/module-external-api/src/main/java/com/eda/external/presentation/MemberApiController.java
+++ Member/module-external-api/src/main/java/com/eda/external/presentation/MemberApiController.java
@@ -3,12 +3,15 @@ package com.eda.external.presentation;
 import com.eda.external.application.port.in.LoginUseCase;
 import com.eda.external.presentation.dto.req.LoginReqDto;
 import com.eda.external.presentation.dto.res.LoginResDto;
+import com.eda.global.common.error.GlobalExceptionHandler;
+import com.eda.global.common.error.exception.BadRequestException;
+import com.eda.global.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import com.eda.common.response.SuccessResponse;
 import com.eda.external.application.port.in.SignUpUseCase;
 import com.eda.external.presentation.dto.req.SignUpReqDto;
 import com.eda.external.presentation.dto.res.SignUpResDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,14 +25,21 @@ public class MemberApiController {
     private final LoginUseCase loginUseCase;
 
     @PostMapping("/signup")
-    public ResponseEntity<SuccessResponse<?>> signup(@RequestBody final SignUpReqDto signUpReqDto) {
+    public ResponseEntity<BaseResponse<?>> signup(@RequestBody final SignUpReqDto signUpReqDto) {
+        GlobalExceptionHandler
+
         final SignUpResDto resDto = SignUpResDto.of(signUpUseCase.signUp(signUpReqDto.toMember()));
-        return SuccessResponse.ok(resDto);
+        return BaseResponse.ok(resDto);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<SuccessResponse<?>> login(@RequestBody final LoginReqDto loginReqDto){
+    public ResponseEntity<BaseResponse<?>> login(@RequestBody final LoginReqDto loginReqDto){
         final LoginResDto resDto = LoginResDto.of(loginUseCase.login(loginReqDto.toMember()));
-        return SuccessResponse.ok(resDto);
+        return BaseResponse.ok(resDto);
+    }
+
+    @GetMapping("/errorTest")
+    public ResponseEntity<BaseResponse<?>> errorTest() {
+        throw new BadRequestException();
     }
 }
