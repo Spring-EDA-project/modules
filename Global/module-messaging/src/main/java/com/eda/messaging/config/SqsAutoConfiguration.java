fcc2601048525fa4196ddf0548ca4a65dea0df22
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2025-03-07 18:09:21 +0900

    [feat] sqs auto configuration 추가

---
+++ Global/module-messaging/src/main/java/com/eda/messaging/config/SqsAutoConfiguration.java
@@ -0,0 +1,49 @@
+package com.eda.messaging.config;
+
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+@Configuration
+@ConditionalOnClass
+@EnableConfigurationProperties(AwsProperties.class)
+public class SqsAutoConfiguration {
+
+    @Autowired
+    private AwsProperties awsProperties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder()
+                .credentialsProvider(() -> new AwsCredentials() {
+                    @Override
+                    public String accessKeyId() {
+                        return awsProperties.getAccessKey();
+                    }
+
+                    @Override
+                    public String secretAccessKey() {
+                        return awsProperties.getSecretKey();
+                    }
+                })
+                .region(Region.of(awsProperties.getRegion()))
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SqsMessageListenerContainerFactory<Object> defaultSqsMessageListenerContainerFactory() {
+        return SqsMessageListenerContainerFactory
+                .builder()
+                .sqsAsyncClient(sqsAsyncClient())
+                .build();
+    }
+}
