62c47555dddb7f7fb762ff2afd35c79ed856e35c
wooing1084 <kmjung1515@naver.com>
2025-03-07 18:19:03 +0900

    faet: global 라이브러리 적용

---
+++ Delivery/module-external-api/src/main/java/com/eda/external/application/in/GetDeliveryUseCase.java
@@ -0,0 +1,5 @@
+package com.eda.external.application.in;
+
+public interface GetDeliveryUseCase {
+
+}
--- Delivery/module-external-api/src/main/java/com/eda/external/presentation/DeliveryApiController.java
+++ Delivery/module-external-api/src/main/java/com/eda/external/presentation/DeliveryApiController.java
@@ -1,6 +1,5 @@
 package com.eda.external.presentation;
 
-import com.eda.common.response.SuccessResponse;
 import com.eda.domain.Delivery;
 import com.eda.external.application.in.UpdateDeliveryUseCase;
 import com.eda.external.application.in.RequestDeliveryUseCase;
@@ -10,6 +9,7 @@ import com.eda.external.presentation.dto.res.DeliveryListResDto;
 import com.eda.external.presentation.dto.res.DeliveryResDto;
 import com.eda.external.presentation.dto.res.DeliveryUpdateResDto;
 import com.eda.external.presentation.dto.res.RequestDeliveryResDto;
+import com.eda.global.common.response.BaseResponse;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -29,33 +29,33 @@ public class DeliveryApiController {
     private final UpdateDeliveryUseCase updateDeliveryUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<SuccessResponse<?>> requestDelivery(@RequestBody final RequestDeliveryReqDto requestDeliveryReqDto) {
+    public ResponseEntity<BaseResponse<?>> requestDelivery(@RequestBody final RequestDeliveryReqDto requestDeliveryReqDto) {
 //        final RequestDeliveryResDto resDto = RequestDeliveryResDto.of(requestDeliveryUseCase.requestDelivery(requestDeliveryReqDto.toDelivery()));
         final RequestDeliveryResDto resDto = RequestDeliveryResDto.of(requestDeliveryReqDto.toDelivery());
-        return SuccessResponse.ok(resDto);
+        return BaseResponse.ok(resDto);
     }
 
     @PatchMapping("/{deliveryId}")
-    public ResponseEntity<SuccessResponse<?>> updateDelivery(@RequestParam(name = "deliveryId") final Long deliveryId,  @RequestBody final DeliveryUpdateReqDto deliveryUpdateReqDto) {
+    public ResponseEntity<BaseResponse<?>> updateDelivery(@RequestParam(name = "deliveryId") final Long deliveryId,  @RequestBody final DeliveryUpdateReqDto deliveryUpdateReqDto) {
 //        final DeliveryUpdateResDto resDto = DeliveryUpdateResDto.of(deliveryUpdateUseCase.updateDelivery(deliveryUpdateReqDto.toDelivery()));
         final DeliveryUpdateResDto resDto = DeliveryUpdateResDto.of(deliveryUpdateReqDto.toDelivery());
-        return SuccessResponse.ok(resDto);
+        return BaseResponse.ok(resDto);
     }
 
     @GetMapping("/{deliveryId}")
-    public ResponseEntity<SuccessResponse<?>> getDelivery(@RequestParam(name = "deliveryId") final Long deliveryId) {
+    public ResponseEntity<BaseResponse<?>> getDelivery(@RequestParam(name = "deliveryId") final Long deliveryId) {
 //        final DeliveryResDto resDto = DeliveryResDto.of(deliveryUpdateUseCase.updateDelivery(deliveryUpdateReqDto.toDelivery()));
         final DeliveryResDto resDto = DeliveryResDto.of(Delivery.builder()
             .id(deliveryId).build());
-        return SuccessResponse.ok(resDto);
+        return BaseResponse.ok(resDto);
     }
 
     @GetMapping("/list/{memberId}")
-    public ResponseEntity<SuccessResponse<?>> getAllDelivery(@RequestParam(name = "memberId") final Long memberId) {
+    public ResponseEntity<BaseResponse<?>> getAllDeliveries(@RequestParam(name = "memberId") final Long memberId) {
 //        final DeliveryUpdateResDto resDto = DeliveryUpdateResDto.of(deliveryUpdateUseCase.updateDelivery(deliveryUpdateReqDto.toDelivery()));
         final DeliveryListResDto resDto = DeliveryListResDto.of(Arrays.asList(Delivery.builder()
             .memberId(memberId).build()));
-        return SuccessResponse.ok(resDto);
+        return BaseResponse.ok(resDto);
     }
 
 }
