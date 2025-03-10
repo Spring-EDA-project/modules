ce202ebe396c87fd00c84e4ce87f11637e5016f6
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2025-03-10 17:25:41 +0900

    [feat] sqsUrls 필드 추가

--- Global/module-messaging/src/main/java/com/eda/messaging/config/AwsProperties.java
+++ Global/module-messaging/src/main/java/com/eda/messaging/config/AwsProperties.java
@@ -1,16 +1,37 @@
 package com.eda.messaging.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
+@Slf4j
+@Configuration
 @ConfigurationProperties(prefix = "eda.aws")
 public class AwsProperties {
     private String accessKey;
     private String secretKey;
     private String region;
 
-    @NestedConfigurationProperty
-    private SnsTopic snstopic;
+    private Map<String, String> snsTopics;
+    private Map<String, String> sqsUrls;
+
+    public Map<String, String> getSnsTopics() {
+        return snsTopics;
+    }
+
+    public void setSnsTopics(Map<String, String> snsTopics) {
+        this.snsTopics = snsTopics;
+    }
+
+    public Map<String, String> getSqsUrls() {
+        return sqsUrls;
+    }
+
+    public void setSqsUrls(Map<String, String> sqsUrls) {
+        this.sqsUrls = sqsUrls;
+    }
 
     public void setAccessKey(String accessKey) {
         this.accessKey = accessKey;
@@ -22,14 +43,6 @@ public class AwsProperties {
 
     public void setRegion(String region) {
         this.region = region;
-    }
-
-    public SnsTopic getSnstopic() {
-        return snstopic;
-    }
-
-    public void setSnstopic(SnsTopic snstopic) {
-        this.snstopic = snstopic;
     }
 
     public String getAccessKey() {
